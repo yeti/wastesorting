520f37827d45ddb45101e4ca7c9c201153984ac0
tonydotsc <tony@yetihq.com>
2016-05-27 16:24:32 -0700

    fixing git issues

---
+++ config/settings/local.py
@@ -0,0 +1,7 @@
+from .base import *
+
+DEBUG = True
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "pP-;?/zuObp:agP~yO*ZdI-~,%F/#IBET#o*xrkF"
+
+ALLOWED_HOSTS = ['*']
